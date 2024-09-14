7be51d56c045049b3768a67ad11d31b1d13c3412
b3m2a1 <b3m2a1@gmail.com>
2024-09-14 13:48:36 -0500

    Usage info

--- tunnels/vscode/postconnect.sh
+++ tunnels/vscode/postconnect.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+echo "########################################################################" >> $SESSION_FILE
+echo "##############          TUNNEL USAGE INSTRUCTIONS         ##############" >> $SESSION_FILE
 echo "Open http://localhost:$HOST_PORT and supply the password listed below" >> $SESSION_FILE
 cat ~/.config/code-server/config.yaml >> $SESSION_FILE
+echo "In VS Code terminals, start by running `source $TUNNEL_ENV_FIlE`"  >> $SESSION_FILE
+echo "If necessary, to load the same conda env, use `conda activate $CONDA_ENVIRONMENT`"  >> $SESSION_FILE
+
 tail -f $SESSION_FILE
--- tunnels/vscode/sbatch_script.sh
+++ tunnels/vscode/sbatch_script.sh
@@ -13,7 +13,5 @@ fi
 # Run VS Code container with singularity on default port
 set -a
 mkdir -p /scratch/user/$VSCODE_USER/.config
-echo "In VS Code terminals, start by running `source $TUNNEL_ENV_FIlE`"
-echo "If necessary, to load the same conda env, use `conda activate $CONDA_ENVIRONMENT`"
 singularity run $VSCODE_CONTAINER
 echo "Exited singularity"
