281fae8b9d11a0d03cec81ca000c7a34fdfe4087
b3m2a1 <b3m2a1@gmail.com>
2024-09-14 13:05:37 -0500

    Update postconnect.sh

--- tunnels/vscode/postconnect.sh
+++ tunnels/vscode/postconnect.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
+echo "Open http://localhost:$HOST_PORT and supply the password listed below" >> $SESSION_FILE
 cat ~/.config/code-server/config.yaml >> $SESSION_FILE
 tail -f $SESSION_FILE
